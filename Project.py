2704b76a046bcfb8886524c4b8ce9c953ecadc92
Sarahkel <35706251+Sarahkel@users.noreply.github.com>
2018-04-19 22:22:47 +0100

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy import stats
 
 iris = pd.read_csv('iris.csv')
-iris.columns = ['sepal_length','sepal_width','petal_length','petal width','species']
+iris.columns = ['sepal_length','sepal_width','petal_length','petal_width','species']
 
 # http://pandas.pydata.org/pandas-docs/stable/10min.html
 # https://www.scipy.org/getting-started.html
