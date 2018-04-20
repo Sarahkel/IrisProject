cfaef9608d959f025fc04c08d9bda91bbf944386
Sarahkel <35706251+Sarahkel@users.noreply.github.com>
2018-04-20 21:41:20 +0100

    + Scripts and function for basic data exploration

--- Project.py
+++ Project.py
@@ -19,6 +19,44 @@ setosa = iris[iris['species']=='Iris-setosa']
 virginica = iris[iris['species']=='Iris-virginica']
 versicolor = iris[iris['species']=='Iris-versicolor']
 
+# Chapter3: BASIC STATS, data exploration
+
+# Commands to be used in IPython, for interactive exploration (else needs to be printed):
+iris # have a look at the data set as a whole (however, python might hide some rows due to length)
+iris.shape # displays rows and columns
+iris.head(5) # show top 5 rows of data
+iris.tail(5) # show last 5 rows of data
+# As IPython will shorten the data set you might want to double check how many species are in the data:
+set(iris['species']) # set is an in-built function in Python which will show you unique values of a given list
+iris['species'].value_counts()
+iris.describe() # returns basic stats about the dataframe
+iris.mean() # returns mean values for columns
+setosa.mean()
+virginica.mean()
+versicolor.mean()
+iris.cov() # returns co-variance between columns
+iris.corr() # returns correlation between columns
+
+# Python function to print out a summary of some handy stats for a dataset x (use x = iris for the purpose of this project)
+def explore(x):
+    print('To begin, lets find out how many rows and columns our dataframe x has:')
+    print(x.shape)
+    print('Below you can see the first 5 rows of the dataframe x')
+    print(x.head(5))
+    print()
+    print('Below you can see the last 5 rows of the dataframe x')
+    print(x.tail(5))
+    print()
+    print('Below you can see the unique values for the column -species- and how many instances of these are found:')
+    print(set(x['species'])) # change this for any column you are interested in
+    print(x['species'].value_counts()) # change this for any column you are interested in
+    print()
+    print('The following provides you with some basic stats about the dataframe:')
+    print(x.describe()) # returns basic stats about the dataframe
+# Not really universally usable. Maybe find more elegant solution for species column
+
+
+
 # Tutorials and useful documentation concerning used packages:
 # http://pandas.pydata.org/pandas-docs/stable/10min.html
 # https://www.scipy.org/getting-started.html
