a86d3f910803b6cbf057fac3295226d816ea502f
Sarahkel <35706251+Sarahkel@users.noreply.github.com>
2018-04-29 21:08:37 +0100

    Format to adapt to chapter

--- VIsuals.py
+++ VIsuals.py
@@ -1,6 +1,7 @@
 # Sarah Scholz, 19/04/2018
 # Visualisation of Iris Data Set
 
+# Chapter 3
 # IMPORT relevant packages, libraries etc.
 import pandas as pd 
 import matplotlib.pyplot as plt 
@@ -8,12 +9,14 @@ import seaborn as sb
 import numpy as np 
 from scipy import stats
 
+# Chapter 4
 # PREPARING THE DATA
 # Read file and format columns
 iris = pd.read_csv('iris.csv', header = None) #include header=None if you wish to assign column names as in the next line. I initially forgot this and ended up with my data set one line short
 iris.columns = ['sepal_length','sepal_width','petal_length','petal_width','species']
 iris.dropna() #cleanup: delete rows with missing values
 
+# From Chapter 5
 # Split per species, so you can run tests on individual species, adapted from: https://www.kaggle.com/willvegapunk/iris-data-set
 setosa = iris[iris['species']=='Iris-setosa']
 virginica = iris[iris['species']=='Iris-virginica']
@@ -27,24 +30,33 @@ sb.boxplot(x='species', y='sepal_length', data=iris) # boxplot
 
 # Overview
 sb.boxplot(data = iris, orient = 'h') # as per https://seaborn.pydata.org/generated/seaborn.boxplot.html
-sb.barplot(data = iris, orient = 'v)
-
-# Combine 2 plots
+sb.barplot(data = iris, orient = 'v) # not used in favor of boxplot
+           
+# Swarmplots
+sb.swarmplot(x='species', y='petal_width', data=iris)
+sb.swarmplot(x='species', y='petal_length', data=iris)
+sb.swarmplot(x='species', y='sepal_length', data=iris)
+sb.swarmplot(x='species', y='sepal_width', data=iris)
+           
+# Layered Swarmplot
+sb.swarmplot(color = 'red', data = setosa, orient = 'v')
+sb.swarmplot(color = 'blue', data = virginica, orient = 'v')
+sb.swarmplot(color = 'green', data = versicolor, orient = 'v')
+           
+# Combine 2 plots (not used in this form)
 sb.violinplot(x='species', y='sepal_length', data=iris)
 sb.swarmplot(x='species', y='sepal_length', data=iris)
 # Issue is that data points aren't well visible. Solution:
 sb.violinplot(x='species', y='sepal_length', data=iris)
 sb.swarmplot(x='species', y='sepal_length', data=iris, color='w')
-
+# Other option
 sb.pairplot(iris)
            
-# Combination Swarmplot
-sb.swarmplot(color = 'red', data = setosa, orient = 'v')
-sb.swarmplot(color = 'blue', data = virginica, orient = 'v')
-sb.swarmplot(color = 'green', data = versicolor, orient = 'v')
+# Swarmplots for Sepal and Petal separately
+sb.swarmplot(x='sepal_width', y='sepal_length', hue = 'species', data=iris)
+sb.swarmplot(x='petal_width', y='petal_length', hue = 'species', data=iris)
            
-# Correlation
+# Vosualising Correlation
 sb.jointplot(x='petal_width', y='petal_length', data=iris, kind = 'reg') # strong positive correlation
-sb.jointplot(x='petal_length', y='sepal_width', data=iris, kind = 'reg') # not a positive correlation
 
 print('done!')
